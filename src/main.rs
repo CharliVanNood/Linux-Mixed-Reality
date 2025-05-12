99b31438fe895262290b6c88b4fa6ea3dd34ed7d
Charli <charli.vannood@gmail.com>
2025-05-12 21:52:29 +0200

    bus identifying

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use rusb::{Context, UsbContext};
+
 fn main() {
-    println!("Hello, world!");
+    let context = Context::new().unwrap();
+
+    let devices = context.devices().unwrap();
+    println!("Found {} devices", devices.len());
+
+    for device in devices.iter() {
+        let desc = device.device_descriptor().unwrap();
+
+        println!(
+            "Bus {:03} Device {:03} ID {:04x}:{:04x}",
+            device.bus_number(),
+            device.address(),
+            desc.vendor_id(),
+            desc.product_id()
+        );
+    }
 }
