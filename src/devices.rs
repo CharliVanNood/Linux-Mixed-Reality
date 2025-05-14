f643cacdd5a389120fb4c0cfa8ba67ca8aa3a023
Charli <charli.vannood@gmail.com>
2025-05-14 11:02:55 +0200

    data reading

--- src/devices.rs
+++ src/devices.rs
@@ -1,17 +1,22 @@
 use rusb::{Context, UsbContext};
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
+
+use crate::devices;
 
 pub struct Devices {
-    devices: HashSet<(u8, u8, u16, u16)>
+    devices: HashSet<(u8, u8, u16, u16)>,
+    selected_devices: Vec<((u8, u8, u16, u16), Vec<u32>)>
 }
 impl Devices {
     pub fn new() -> Self {
-        Self { devices: HashSet::new() }
+        Self {
+            devices: HashSet::new(),
+            selected_devices: Vec::new()
+        }
     }
 
     pub fn check_devices(&mut self) {
         let context = Context::new().unwrap();
-
         let devices = context.devices().unwrap();
 
         print!("\n\n\n\n\n\n\n\n");
@@ -20,6 +25,10 @@ impl Devices {
 
             if !self.devices.contains(&(device.bus_number(), device.address(), desc.vendor_id(), desc.product_id())) {
                 self.devices.insert((device.bus_number(), device.address(), desc.vendor_id(), desc.product_id()));
+
+                if desc.vendor_id() == 0x048d && desc.product_id() == 0xc981 {
+                    self.selected_devices.push(((device.bus_number(), device.address(), desc.vendor_id(), desc.product_id()), Vec::new()))
+                }
             }
 
             println!(
@@ -33,5 +42,23 @@ impl Devices {
         }
 
         println!("-------------------- {} Devices -------------------", devices.len());
+    }
+
+    pub fn check_usb(&mut self) {
+        let context = Context::new().unwrap();
+        let devices = context.devices().unwrap();
+
+        for device_checking in devices.iter() {
+            let desc = device_checking.device_descriptor().unwrap();
+
+            for device_item in &self.selected_devices {
+                if device_item.0.0 != device_checking.bus_number() ||
+                    device_item.0.1 != device_checking.address() ||
+                    device_item.0.2 != desc.vendor_id() ||
+                    device_item.0.3 != desc.product_id() { continue; }
+                let device = &device_checking;
+                println!("data: {:?}", device.as_raw())
+            }
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,12 @@ mod devices;
 fn main() {
     let mut devices = devices::Devices::new();
 
+    let mut check_index = 0;
     loop {
-        devices.check_devices();
-        thread::sleep(Duration::from_millis(500));
+        if check_index % 500 == 0 { devices.check_devices(); }
+        devices.check_usb();
+
+        check_index += 1;
+        thread::sleep(Duration::from_millis(1));
     }
 }
